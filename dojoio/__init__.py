c1db8b5f6d9d0822ca232bfe6060cbdc59b2b211
urakubo <hurakubo@gmail.com>
2019-10-29 17:51:38 +0900

    Update __init__.py

--- dojoio/__init__.py
+++ dojoio/__init__.py
@@ -4,7 +4,6 @@ from ImportImgSeg import ImportImgSeg
 import Miscellaneous as m
 from np2imgo import Imgo
 from np2sego import Sego
-from Params import Params
 from SaveChanges import SaveChanges
 import tifffile
 
