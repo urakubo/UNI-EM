078da3e15401cdcf2963c11d2e5eeda64403bcce
esnya <esnya@nekometer.com>
2020-03-01 20:30:49 +0900

    Update socket stubs to allow entry into multiple rooms

--- annotator/Annotator/sio.py
+++ annotator/Annotator/sio.py
@@ -1,4 +1,4 @@
-# Socket.ioイベント処理 
+# Socket.ioイベント処理
 
 from urllib.parse import parse_qs
 import socketio
@@ -8,12 +8,10 @@ from annotator.Annotator import room
 sio = socketio.AsyncServer(async_mode='tornado')
 
 @sio.event
-async def update(sid, data):
-  session = await sio.get_session(sid)
-  room_id = session['room_id']
-
+async def update(sid, room_id, data):
+  # ToDo: 入力条件
+  print('Update', room_id, data)
   room.update_room(room_id, data)
-
   await sio.emit('update', room.get_room(room_id), room=room_id, skip_sid=sid)
 
 @sio.event
@@ -21,13 +19,17 @@ async def disconnect_request(sid):
   await sio.disconnect(sid)
 
 @sio.event
-async def connect(sid, environ):
-  room_id = parse_qs(environ.get('QUERY_STRING'))['room_id'][0] # ToDo: エラー処理
-  
-  await sio.save_session(sid, { 'room_id': room_id })
+async def enter(sid, room_id):
+  # ToDo: 入力条件
+  print('Enter', sid, room_id)
   sio.enter_room(sid, room_id)
-
   await sio.emit('current', room.get_room(room_id), room=sid)
+
+@sio.event
+def leave(sid, room_id):
+  # ToDo: 入力条件
+  print('Leave', sid, room_id)
+  sio.leave_room(sid, room_id)
 
 @sio.event
 def disconnect(sid):
