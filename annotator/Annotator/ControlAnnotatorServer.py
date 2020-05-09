1f80ded2f89bfdaadfcf94ece8eeab0481de3739
urakubo <hurakubo@gmail.com>
2020-05-10 01:29:57 +0900

    Cleanup

--- annotator/Annotator/ControlAnnotatorServer.py
+++ annotator/Annotator/ControlAnnotatorServer.py
@@ -5,10 +5,6 @@
 
 import sys, os, time, errno
 import threading
-import sqlite3
-import json
-from marching_cubes import march
-from stl import mesh
 from os import path, pardir
 
 ##
@@ -76,51 +72,5 @@ class ControlAnnotatorServer:
         self.u_info.annotator_thread = None
 
 
-
-
-class GenerateAnnotatorFolder:
-
-    ###
-    def __init__(self, u_info):
-
-        ## User info
-        self.u_info = u_info
-
-        ## Makedir
-        if os.path.isdir(self.u_info.surfaces_path) == False:
-        	os.makedirs(self.u_info.surfaces_path)
-        	os.makedirs(self.u_info.surfaces_whole_path)
-
-        ## Load color file
-        colordata = m.load_hdf5(self.u_info.annotator_color_map_file, self.u_info.hdf_color_name) 
-        colnum = colordata.shape[0];
-
-
-        ##
-        ## Load database file
-        ##
-
-        query = "select * from segmentInfo;"
-        con = sqlite3.connect( self.u_info.annotator_segment_info_db_file )
-        cur = con.cursor()
-        cur.execute( query ) # Obtain max id
-        #data = cur.fetchone()
-        data = cur.fetchall()
-        con.close()
-
-        keys = ['id', 'name', 'size', 'confidence']
-        data_dict = [dict(zip(keys, valuerecord)) for valuerecord in data]
-
-        for i, datum_dict in enumerate(data_dict):
-            id  = datum_dict['id']
-            if id >= colnum:
-                col = {'r': 128, 'g': 128, 'b': 128, 'act': 0}
-            else:
-                col = {'r': int(colordata[id][0]), 'g': int(colordata[id][1]),  'b': int(colordata[id][2]),  'act': 0}
-            data_dict[i].update(col)
-
-
-        with open(os.path.join(self.u_info.surfaces_path,"segmentInfo.json"), 'w') as f:
-            json.dump(data_dict, f, indent=2, ensure_ascii=False)
 
 
