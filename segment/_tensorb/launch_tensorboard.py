37645f7f52a98e66add44db0e2924d1cac7a5cca
urakubo <hurakubo@gmail.com>
2021-05-21 16:43:34 +0900

    Update launch_tensorboard.py

--- segment/_tensorb/launch_tensorboard.py
+++ segment/_tensorb/launch_tensorboard.py
@@ -11,13 +11,13 @@ from tensorboard import program
 from tensorboard.compat import tf
 from tensorboard.plugins import base_plugin
 from tensorboard.util import tb_logging
-# import socket
-import time
-
-logger = tb_logging.get_logger()
-program.setup_environment()
 
 from argparse import ArgumentParser
+# import socket
+import time
+from os import path, pardir
+main_dir = path.abspath(path.dirname(sys.argv[0]))  # Dir of main
+
 usage = 'Usage: python tensorb [--logdir] [--host]'
 argparser = ArgumentParser(usage=usage)
 argparser.add_argument('--logdir', type=str,
@@ -28,7 +28,14 @@ args = argparser.parse_args()
 
 argv=[None, '--logdir', args.logdir,'--host', args.host]
 
-tb = program.TensorBoard(default.get_plugins() + default.get_dynamic_plugins(), program.get_default_assets_zip_provider())
+logger = tb_logging.get_logger()
+program.setup_environment()
+
+# See
+# Tensorboard/program.py: get_default_assets_zip_provider
+
+webfiles = path.join(main_dir,'tensorboard','webfiles.zip')
+tb = program.TensorBoard(default.get_plugins() + default.get_dynamic_plugins(), lambda: open(webfiles, 'rb'))
 tb.configure(argv=argv)
 tb.launch()
 try:
