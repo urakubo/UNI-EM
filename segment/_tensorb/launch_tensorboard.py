94d19217ff96e0f38453c3f1c266e14a47675545
urakubo <hurakubo@gmail.com>
2021-09-12 19:15:11 +0900

    Update launch_tensorboard.py

--- segment/_tensorb/launch_tensorboard.py
+++ segment/_tensorb/launch_tensorboard.py
@@ -2,12 +2,14 @@
 # Modified from :
 # C:\Users\uraku\AppData\Local\Programs\Python\Python38\Lib\site-packages\tensorboard\main.py
 
+import os
 from os import path, pardir
 main_dir = path.abspath(path.dirname(sys.argv[0]))  # Dir of main
+webfiles = path.join(main_dir,'tensorboard','webfiles.zip')
 
 
 # pylint: disable=g-import-not-at-top
-import os
+
 
 # Disable the TF GCS filesystem cache which interacts pathologically with the
 # pattern of reads used by TensorBoard for logdirs. See for details:
@@ -16,49 +18,36 @@ import os
 os.environ['GCS_READ_CACHE_DISABLED'] = '1'
 # pylint: enable=g-import-not-at-top
 
-import sys
-
+from absl import app
 from tensorboard import default
+from tensorboard import main_lib
 from tensorboard import program
-from tensorboard.compat import tf
 from tensorboard.plugins import base_plugin
+from tensorboard.uploader import uploader_subcommand
 from tensorboard.util import tb_logging
-
 
 logger = tb_logging.get_logger()
 
+
+
 def run_main():
-  """Initializes flags and calls main()."""
-  program.setup_environment()
+    """Initializes flags and calls main()."""
+    main_lib.global_init()
 
-  if getattr(tf, '__version__', 'stub') == 'stub':
-    print("TensorFlow installation not found - running with reduced feature set.",
-          file=sys.stderr)
-
-  ##
-  webfiles = path.join(main_dir,'tensorboard','webfiles.zip')
-  ##
-
-  tensorboard = program.TensorBoard(
-      default.get_plugins() + default.get_dynamic_plugins(),
-      lambda: open(webfiles, 'rb'))
-  try:
-    from absl import app
-    # Import this to check that app.run() will accept the flags_parser argument.
-    from absl.flags import argparse_flags
-    app.run(tensorboard.main, flags_parser=tensorboard.configure)
-    raise AssertionError("absl.app.run() shouldn't return")
-  except ImportError:
-    pass
-  except base_plugin.FlagsError as e:
-    print("Error: %s" % e, file=sys.stderr)
-    sys.exit(1)
-
-  tensorboard.configure(sys.argv)
-  sys.exit(tensorboard.main())
+    tensorboard = program.TensorBoard(
+        plugins=default.get_plugins(),
+        assets_zip_provider=lambda: open(webfiles, 'rb'),
+        subcommands=[uploader_subcommand.UploaderSubcommand()],
+    )
+    try:
+        app.run(tensorboard.main, flags_parser=tensorboard.configure)
+    except base_plugin.FlagsError as e:
+        print("Error: %s" % e, file=sys.stderr)
+        sys.exit(1)
 
 
-if __name__ == '__main__':
-  run_main()
+if __name__ == "__main__":
+    run_main()
+
 
 
