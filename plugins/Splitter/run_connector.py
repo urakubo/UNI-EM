af13efd63451ae8f7c8b2dea89ec7c37cb2ee4e8
urakubo <hurakubo@gmail.com>
2022-08-13 23:21:34 +0900

    Update run_connector.py

--- plugins/Splitter/run_connector.py
+++ plugins/Splitter/run_connector.py
@@ -36,9 +36,9 @@ def load_volume( folder, i_slices, ext, im_dtype, size_hh, size_ww ):
 	return volume
 
 
-def connector(reference, target):
+def connector(reference, target, threshold):
 
-	TH = 0.8
+	TH = threshold / 100
 	connection = []
 	for id_ref, count_ref in zip( *np.unique(reference, return_counts=True) ) :
 		t = target[reference == id_ref]
@@ -101,7 +101,7 @@ class SimpleMerger():
 			m.save_tifc(image2d, filename, colormap)
 		else:
 			print('Segmentation was not saved.')
-
+	
 	
 	def load_seg_volume(self, ih, iw, iz, size_hh, size_ww, size_zz):
 		seg_dir = os.path.join(self.split_folder, self.seg_folder, '{:03d}_{:03d}_{:03d}'.format(ih,iw,iz) )
@@ -114,9 +114,9 @@ class SimpleMerger():
 		return os.path.join(self.split_folder, self.seg_folder, '{:03d}_{:03d}_{:03d}'.format(ih,iw,iz) )
 	
 	
-	
 	def _Run(self, parent, params, comm_title):
 		#
+		self.threshold = params['Overlap for connected components']
 		self.merge_folder   = params['Merged segmentation folder']
 		self.merge_filetype = params['Merged segmentation filetype']
 		self.z_info       = params['z_info']
@@ -167,8 +167,155 @@ class SimpleMerger():
 		return True
 		
 		
+	def connector(self):
 		
-	
+		
+		# print('ids_unique ', ids_unique)
+		
+		# connector
+		self.g = nx.Graph()
+		
+		print('connection in z')
+		size_hh = (0, self.split_size_h)
+		size_ww = (0, self.split_size_w)
+		for ih, iw, iz in product( range(self.num_h), range(self.num_w), range(self.num_z-1) ):
+			print('ih, iw, iz ', ih, iw, iz )
+			### Connection z
+			v_sub_ref  = self.load_seg_volume(ih, iw,   iz, size_hh, size_ww, [-self.overlap_size_z-1,-1])
+			v_sub_targ = self.load_seg_volume(ih, iw, iz+1, size_hh, size_ww, [0,self.overlap_size_z])
+			#print('v_sub_ref.shape  ', v_sub_ref.shape)
+			#print('v_sub_targ.shape ', v_sub_targ.shape)
+			ids_connector = connector(v_sub_ref, v_sub_targ, self.threshold)
+			self.g = register_graph(self.g, ids_connector, (ih, iw, iz), (ih, iw, iz+1))
+
+
+		print('connection in h')
+		size_hh_ref  = (self.split_size_h-self.overlap_size_h, self.split_size_h)
+		size_hh_targ = (0, self.overlap_size_h)
+		size_ww = (0, self.split_size_w)
+		for ih, iw, iz in product( range(self.num_h-1), range(self.num_w), range(self.num_z) ):
+			print('ih, iw, iz ', ih, iw, iz )
+			### Reference volume
+			v_sub_ref  = self.load_seg_volume(  ih, iw, iz, size_hh_ref , size_ww, [0,-1] )
+			v_sub_targ = self.load_seg_volume(ih+1, iw, iz, size_hh_targ, size_ww, [0,-1] )
+			ids_connector = connector(v_sub_ref, v_sub_targ)
+			self.g = register_graph(self.g, ids_connector, (ih, iw, iz), (ih+1, iw, iz))
+
+
+		print('connection in w')
+		size_hh = (0, self.split_size_h)
+		size_ww_ref  = (self.split_size_w-self.overlap_size_w, self.split_size_w)
+		size_ww_targ = (0, self.overlap_size_w)
+		for ih, iw, iz in product( range(self.num_h), range(self.num_w-1), range(self.num_z) ):
+			print('ih, iw, iz ', ih, iw, iz )
+			### Reference volume
+			v_sub_ref  = self.load_seg_volume(ih,   iw, iz, size_hh, size_ww_ref , [0,-1])
+			v_sub_targ = self.load_seg_volume(ih, iw+1, iz, size_hh, size_ww_targ, [0,-1])
+			ids_connector = connector(v_sub_ref, v_sub_targ)
+			self.g = register_graph(self.g, ids_connector, (ih, iw, iz), (ih, iw+1, iz))
+		
+		##
+		## from graph 'g' to dict ids_unique
+		##
+		print('Check cross split-volume objects.')
+		id_color = 0
+		for subs in sorted(nx.connected_components(self.g), key=len, reverse=True):
+			#print('id_color', id_color)
+			for sub in subs:
+				self.g.nodes[sub]['id_color'] = id_color
+			id_color += 1
+		
+		print('Check split-volume specific objects.')
+		self.ids_unique = np.empty((self.num_h, self.num_w, self.num_z), dtype=object)
+		connected_id_list = list(con.g.nodes)
+		for ih, iw, iz in product( range(self.num_h), range(self.num_w), range(self.num_z) ):
+			seg_dir = self.obtain_seg_dir(ih, iw, iz)
+			ids  = {}
+			for id in load_unique_ids( seg_dir, self.i_slices[ih,iw,iz], self.ext, self.im_dtype ):
+				node_name = '{:03d}_{:03d}_{:03d}_{:d}'.format(ih,iw,iz,id)
+				if node_name not in connected_id_list:
+					ids[id] = id_color
+					id_color += 1
+				else:
+					ids[id] = self.g.nodes[node_name]['id_color']
+			self.ids_unique[ih,iw,iz] = ids
+		self.num_color = id_color
+		print( 'self.num_color ', self.num_color )
+		
+		
+	def merger(self):
+		##
+		##
+		##
+		merged_im_dtype = np.uint16
+		merged_im_size  = (self.im_size_h, self.im_size_w)
+
+		if  'color' in self.merge_filetype:
+			colormap = np.random.randint(255, size=(self.num_color+2, 3), dtype=np.uint8)
+			colormap[0,:] = 0
+		else:
+			colormap = None
+
+		if  'TIFF' in self.merge_filetype:
+			merged_ext = 'tif'
+		elif 'PNG' in self.merge_filetype:
+			merged_ext = 'png'
+		else:
+			print('Internal error about the output filetype.')
+			exit()
+		##
+		##
+		##
+		self._create_merge_folder()
+		assign_panel_hs, assign_merged_hs = m.assign_regions_for_merge(self.im_size_h, self.split_size_h, self.overlap_size_h)
+		assign_panel_ws, assign_merged_ws = m.assign_regions_for_merge(self.im_size_w, self.split_size_w, self.overlap_size_w)
+		z_info = self.z_info
+		if len(z_info) >= 2:
+			z_info = [z_info[0][:-self.overlap_size_z_2]] + \
+				[ z_sub[self.overlap_size_z_2:-self.overlap_size_z_2 ] for z_sub in z_info[1:-1] ] + \
+				[ z_info[-1][self.overlap_size_z_2:] ]
+		else:
+			pass
+		if self.reflect_pad == True:
+			 z_info[0] = z_info[0][self.overlap_size_z:]
+		##
+		##
+		##
+		for iz in  range(self. num_z ):
+			for z_sub_info in z_info[iz]:
+				#print('iz, ', iz)
+				merged_image = np.zeros( merged_im_size, dtype = merged_im_dtype )
+				for iw, ih in product( range( self.num_w ), range( self.num_h ) ):
+					file_panel = os.path.join( self.obtain_seg_dir(ih,iw,iz), '{:04d}.{}'.format(z_sub_info['i_slice'], self.ext) )
+					cropped_image = m.read_seg_image(file_panel, self.ext)
+					mh = assign_merged_hs[ih]
+					ph = assign_panel_hs[ih]
+					mw = assign_merged_ws[iw]
+					pw = assign_panel_ws[iw]
+					tmp = cropped_image[ph[0]:ph[1], pw[0]:pw[1]]
+					ids = self.ids_unique[ih,iw,iz]
+					#print('ids ', ids)
+					imgArray = np.zeros_like(tmp, dtype=merged_im_dtype)
+					for i in range(tmp.shape[0]):
+						for j in range(tmp.shape[1]):
+							# print('tmp[i, j]' , tmp[i, j] )
+							imgArray[i, j] = ids[tmp[i, j]]
+					merged_image[mh[0]:mh[1], mw[0]:mw[1]] = imgArray
+					
+				if self.reflect_pad == True:
+					merged_image = merged_image[self.overlap_size_h:-self.overlap_size_h, \
+						self.overlap_size_w:-self.overlap_size_w]
+
+				# print('np.unique(merged_image) ' , np.unique(merged_image, return_counts=True) )
+
+				base_name = os.path.splitext(os.path.basename(z_sub_info['image_file']))[0]
+				filename_merged = os.path.join( self.merge_folder, '{}.{}'.format( base_name, merged_ext ) )
+				print('filename_merged: ', filename_merged)
+				# m.imwrite(file_merged, merged_image)
+				self.save_image(merged_image, filename_merged, self.merge_filetype, colormap = colormap)
+		##
+		##
+		##
 	
 	def __init__(self, u_info):
 		##
@@ -178,11 +325,13 @@ class SimpleMerger():
 		self.title = 'Splitter'
 
 		self.tips = [
+		                'Threshold for connected components',
 		                'Merged segmentation folder',
                         'Output filetype'
 		                ]
 
 		self.args = [
+		                ['Overlap for connected components', 'SpinBox', [0, 80, 100]],
 		                ['Merged segmentation folder (Empty)',  'SelectEmptyModelFolder', 'OpenEmptyModelFolder'],
                         ['Merged segmentation Filetype', 'ComboBox', ["8-bit color PNG", "16-bit gray scale PNG", "8-bit gray scale PNG", 
                                                          "8-bit color TIFF", "16-bit gray scale TIFF", "8-bit gray scale TIFF"]]
